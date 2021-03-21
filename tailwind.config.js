720f9183ebaa2035f34593567be249f2db2e0b27
Marina Oliveira <marinaoliveira.r07@gmail.com>
2021-03-21 18:25:23 -0300

    fix.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./templates/**/*.html"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     colors: {
